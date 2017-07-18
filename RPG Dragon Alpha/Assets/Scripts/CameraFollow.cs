34418f4e33756676dc8622fd9a09960ea0789bc4
DESKTOP-4646T2M\borisfilipovic <boris.filipovic1@gmail.com>
2017-07-18 21:34:21 +0200

    Added follow camera script.

---
+++ RPG Dragon Alpha/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+    // Properties.
+
+    GameObject player;
+
+	// Use this for initialization
+	void Start () {
+        // Find player gameobject.
+        player = GameObject.FindGameObjectWithTag("Player");
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		// Set camera position to be the same as player position on every frame.
+        if (player != null)
+        {
+            transform.position = player.transform.position;
+        }
+	}
+}
