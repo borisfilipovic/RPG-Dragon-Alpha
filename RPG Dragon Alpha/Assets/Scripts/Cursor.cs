191e46b6fdbb167607a58a8f774d0049805382d9
DESKTOP-4646T2M\borisfilipovic <boris.filipovic1@gmail.com>
2017-07-19 20:33:50 +0200

    Added camera raycaster.

---
+++ RPG Dragon Alpha/Assets/Scripts/Cursor.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cursor : MonoBehaviour {
+
+    // Properties.
+
+    CameraRaycaster cameraRaycaster;
+
+	// Use this for initialization
+	void Start () {
+        // Find raycaster camera.
+        findRaycasterCamera();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        print(cameraRaycaster.layerHit);
+	}
+
+    // Find raycaster camera.
+    private void findRaycasterCamera()
+    {
+        cameraRaycaster = GetComponent<CameraRaycaster>();
+    }
+}
